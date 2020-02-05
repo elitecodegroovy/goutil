776729571ad0ef20d94d14f4d95827bf96616837
elitecodegroovy <elite_jigang@163.com>
2020-02-05 23:56:51 +0800

    test case

--- parse_time_test.go
+++ parse_time_test.go
@@ -27,3 +27,22 @@ func TestParseIn(t *testing.T) {
 	//
 	//     t, err := dateparse.ParseIn("3/1/2014", denverLoc)
 }
+
+func TestParseLocal(t *testing.T) {
+	var err error
+	var time1 time.Time
+	if time1, err = ParseLocal("2014/01/15"); err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Printf("time1: %+v \n", time1)
+	fmt.Printf("time1: %+v \n", GetISOStrTime(time1))
+
+	//     denverLoc, _ := time.LoadLocation("Asia/Chongqing)
+	//     time.Local = denverLoc
+	//
+	//     t, err := dateparse.ParseLocal("3/1/2014")
+	//
+	// Equivalent to:
+	//
+	//     t, err := dateparse.ParseIn("3/1/2014", denverLoc)
+}
